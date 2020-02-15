99f53bffb860db20484f3d02c76ff554cc48e11f
Abdulrahman Alattas <rmnattas@gmail.com>
2020-02-15 14:21:25 -0700

    models

--- django_backend/EventManager/models.py
+++ django_backend/EventManager/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class Events(models.Model):
+    title = models.CharField()
+    time = models.DateTimeField()
+    location = models.CharField()
+    description = models.TextField
+    picture = models.models.FileField(upload_to="images/")
+    registration = models.URLField()
+    organizerEmail = models.EmailField()
+    deadline = models.DateTimeField()
+    tags = models.ArrayField(models.models.CharField())
+    faculty = models.CharField()
+    onCampus = models.BooleanField()
+    hasFood = models.BooleanField()
+    isPrice = models.BooleanField()
+
+class Tags(models.Model):
+    tag = models.CharField()
+
+class Faculty(models.Model):
+    faculty = models.CharField()
